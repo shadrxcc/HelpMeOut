536a741278c3ad967888c2ddd5ce410952aac183
shadrxcc <akaadesm@gmail.com>
2023-09-28 16:42:31 +0100

    modify features layout

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -3,7 +3,7 @@ import videorepo from "../assets/videorepo.svg";
 
 const Features = () => {
   return (
-    <div className="bg-white py-20 flex flex-col gap-y-16 px-4 sm:px-8 lg:px-[100px]">
+    <div className="bg-white py-20 flex flex-col sm:gap-y-16 px-4 sm:px-8 lg:px-[100px]">
       <span className="flex justify-center items-center flex-col gap-y-2">
         <h3 className="text-[#141414] place-items-stretch text-[40px] font-bold">
           Features
@@ -13,21 +13,21 @@ const Features = () => {
         </p>
       </span>
 
-      <div className="flex flex-col-reverse sm:flex-row gap-y-8 gap-x-14 items-center">
-        <div className="flex-1 flex flex-col gap-y-12 sm:gap-y-5">
+      <div className="flex flex-col-reverse sm:flex-row gap-y-2 gap-x-14 items-center">
+        <div className="flex-1 flex flex-col gap-y-8 sm:gap-y-5">
           {features.map((feature) => {
             return (
-              <div className="flex gap-x-6 items-baseline" key={feature.id}>
+              <div className="flex flex-col justify-center sm:flex-row gap-x-6 items-center sm:items-baseline" key={feature.id}>
                 <img
                   className="w-[32px]"
                   src={feature.icon}
                   alt={feature.text}
                 />
                 <div className="flex flex-col gap-y-2">
-                  <p className="text-[#1B233D] font-semibold text-[28px]">
+                  <p className="text-[#1B233D] text-center sm:text-start font-semibold text-[28px]">
                     {feature.text}
                   </p>
-                  <p className="text-[#616163] text-[20px]" id="work">
+                  <p className="text-[#616163] text-center sm:text-start text-[20px]" id="work">
                     {feature.desc}
                   </p>
                 </div>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ import Imagegrid from "./imagegrid";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col-reverse gap-y-6 sm:flex-row p-4 bg-white mt-1 sm:p-8 lg:p-[100px] gap-x-8 justify-center mx-auto items-center">
+    <div className="flex flex-col-reverse gap-y-6 sm:flex-row px-4 py-20 bg-white mt-1 sm:px-8 sm:py-20 lg:p-[100px] gap-x-8 justify-center mx-auto items-center">
       <div className="flex-1 flex flex-col gap-y-6 lg:gap-y-12">
         <div className="flex flex-col">
           <h1 className="text-[#141414] leading-[64px] text-2xl font-bold sm:text-[42px] lg:text-[48px] xl:text-[64px]">
