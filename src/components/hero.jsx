7e1a9f23e6b68830282b990ee7650aebd2034e01
shadrxcc <akaadesm@gmail.com>
2023-09-28 16:06:28 +0100

    made hero responsive

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,10 +4,10 @@ import Imagegrid from "./imagegrid";
 
 const Hero = () => {
   return (
-    <div className="flex bg-white mt-1 p-[100px] gap-x-8 justify-center mx-auto items-center">
-      <div className="flex-1 flex flex-col gap-y-12">
+    <div className="flex flex-col-reverse gap-y-6 sm:flex-row p-4 bg-white mt-1 sm:p-8 lg:p-[100px] gap-x-8 justify-center mx-auto items-center">
+      <div className="flex-1 flex flex-col gap-y-6 lg:gap-y-12">
         <div className="flex flex-col">
-          <h1 className="text-[#141414] font-bold text-[64px]">
+          <h1 className="text-[#141414] leading-[64px] text-2xl font-bold sm:text-[42px] lg:text-[48px] xl:text-[64px]">
             Show Them Don’t Just Tell
           </h1>
           <p id="inter" className="text-base leading-7">
@@ -16,7 +16,7 @@ const Hero = () => {
           </p>
         </div>
 
-        <Button className="text-lg w-fit font-medium flex items-center gap-x-3 p-[22px]">
+        <Button className="text-sm sm:text-lg w-fit font-medium flex items-center gap-x-3 p-4 sm:p-[22px]">
           Install HelpMeOut <img src={arrow} alt="" />
         </Button>
       </div>
